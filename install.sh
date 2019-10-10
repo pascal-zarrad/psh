5633846d6332df5847f9d8d1a6d94717980f3f95
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-10 22:41:47 +0200

    Create initial installer file

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#==================================================================
+# Script Name   : psh-installer                                                                                             
+# Description	: Installer script that installs psh (zsh installer
+#                 with default configuration)
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
