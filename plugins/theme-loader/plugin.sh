bf6bdf30daf605f8a2ca5e51b1b726b12fd4a4ce
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 00:00:32 +0200

    Add plugin system for customizations

---
+++ plugins/theme-loader/plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-theme-loader-installer
+# Description	: Plugin that handles the activation of
+#                 default theme for psh.
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+# Set theme to use
+apply_antigen_theme "agnoster"
