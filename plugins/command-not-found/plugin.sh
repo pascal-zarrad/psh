2c4f9827bb63765f47fe27c1f7427567371bada2
Nekwo <nico.just@flagbit.de>
2019-10-13 22:08:31 +0200

    Add zsh plugin for commands that cannot be found

---
+++ plugins/command-not-found/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : command-not-found
+# Description	: Enables oh-my-zsh's command-not-found plugin
+# Args          : -
+# Author       	: Nico Just
+# Email         : nicojust@users.noreply.github.com
+#==================================================================
+
+apply_antigen_bundle "command-not-found"
