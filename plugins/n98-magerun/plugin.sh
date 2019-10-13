92d79a3b09a0849ecd8d80898d7b1cb2484c995a
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:14:12 +0200

    Add n98-magerun plugin

---
+++ plugins/n98-magerun/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-n98-magerun-installer
+# Description	: Enables oh-my-zsh's n98-magerun plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "n98-magerun"
