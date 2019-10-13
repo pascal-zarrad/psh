d2502900904186ff50d23a176370fca7d9f2c46d
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 02:34:41 +0200

    Add aws plugin

---
+++ plugins/aws/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-aws-installer
+# Description	: Enables oh-my-zsh's aws plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "aws"
