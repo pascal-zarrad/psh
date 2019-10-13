3d7b9f1e26b2e06e3853b0a8458bc4add2da0327
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 03:18:27 +0200

    Add pip plugin

---
+++ plugins/pip/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-pip-installer
+# Description	: Enables oh-my-zsh's pip plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "pip"
