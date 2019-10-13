f8fe27ab39f4d76aa28f2d4be0c644927b3b0689
Nekwo <nico.just@flagbit.de>
2019-10-13 22:14:06 +0200

    Add zsh plugins for phpunit & phpcs

---
+++ plugins/php/plugin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : voronkovich/phpcs.plugin.zsh
+# Description	: Enables oh-my-zsh's voronkovich/phpcs.plugin.zsh plugin
+# Args          : -
+# Author       	: Nico Just
+# Email         : nicojust@users.noreply.github.com
+#==================================================================
+
+apply_antigen_bundle "voronkovich/phpcs.plugin.zsh"
+apply_antigen_bundle "voronkovich/phpunit.plugin.zsh"
