09f56c04f556c6245c75cf1e8f5d066a29e67dec
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 03:00:59 +0200

    Add encode64 plugin

---
+++ plugins/encode64/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-common-encode64-installer
+# Description	: Enables oh-my-zsh's encode64 plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "encode64"
