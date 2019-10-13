a7fa7e2a58d595b9c4251a7552afd562cce10783
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 02:44:09 +0200

    Add catimg plugin

---
+++ plugins/catimg/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-catimg-installer
+# Description	: Enables oh-my-zsh's catimg plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "catimg"
