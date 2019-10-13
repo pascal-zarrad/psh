9deeb8edb5e33c2b3f1c898ace73c2d706158c87
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 03:08:18 +0200

    Add gradle plugin

---
+++ plugins/gradle/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-gradle-installer
+# Description	: Enables oh-my-zsh's gradle plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "gradle"
