2e6d541b2bc09ab82a675fd4f3f2c75a3eb35883
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 02:02:00 +0200

    Add temp workaround for wsl permission problem

---
+++ fix-wsl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : fix-wsl
+# Description	: Script to fix permission problems on WSL
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+if [ -d "${HOME}/.antigen/bundles" ]
+    then
+        echo "Fixing permissions of ${HOME}/.antigen/bundles..."
+        chmod -R 755 "${HOME}/.antigen/bundles"
+        if [ "$?" -eq 0 ]
+            then
+                echo "Succesfully fixed permissions of ${HOME}/.antigen/bundles"
+            else
+                echo "Failed to fix permissions of ${HOME}/.antigen/bundles"
+        fi
+    else
+        echo "${HOME}/.antigen/bundles does not exist, you first have to launch the fully configured"
+        echo "zsh at least once after running this fixer."
+fi
