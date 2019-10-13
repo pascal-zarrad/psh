ff870a7c9d66e91f04ebea01315b54f3db8debe0
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 02:51:42 +0200

    Add composer plugin

---
+++ plugins/composer/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-composer-installer
+# Description	: Enables oh-my-zsh's composer plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "composer"
