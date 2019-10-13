9530f7833641e9199affd0066249c8632e776013
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:16:18 +0200

    Add node plugin

---
+++ plugins/node/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-node-installer
+# Description	: Enables oh-my-zsh's node plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "node"
