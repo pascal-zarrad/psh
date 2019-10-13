a37a3882764c56f07bac8479ea7a81da230ccec1
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:07:30 +0200

    Add golang plugin

---
+++ plugins/golang/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-golang-installer
+# Description	: Enables oh-my-zsh's golang plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "golang"
