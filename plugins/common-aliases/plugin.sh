f3c343bd122d3df5f4508b17349667dc734b463b
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 02:49:24 +0200

    Add common-aliases plugin

---
+++ plugins/common-aliases/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-common-aliases-installer
+# Description	: Enables oh-my-zsh's common aliases plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "common-aliases"
