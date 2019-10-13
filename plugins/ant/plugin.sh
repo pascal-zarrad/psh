a9b444af952c7def92df025e5a1835cecadd5a66
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 02:32:39 +0200

    Add ant plugin

---
+++ plugins/ant/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-ant-installer
+# Description	: Enables oh-my-zsh's ant plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "ant"
