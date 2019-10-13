60cf2ca8ecaa4dbc16ee854a4556b1ab081d4d1b
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 02:38:58 +0200

    Add bgnotify plugin

---
+++ plugins/bgnotify/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-bgnotify-installer
+# Description	: Enables oh-my-zsh's bgnotify plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "bgnotify"
