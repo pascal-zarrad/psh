1f40e829a470410026504b3146cde7653e2783d0
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:02:32 +0200

    Add extract plugin

---
+++ plugins/extract/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-extract-installer
+# Description	: Enables oh-my-zsh's extract plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "extract"
