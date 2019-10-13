ee272e8de02dcac145d8d5a3393594ae1a485620
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 03:12:58 +0200

    Add mvn plugin

---
+++ plugins/mvn/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-mvn-installer
+# Description	: Enables oh-my-zsh's mvn plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "mvn"
