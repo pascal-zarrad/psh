26b0f38da4d94263bf45cd763ff4844a8e961057
pascal-zarrad <P.Zarrad@web.de>
2019-10-13 03:24:38 +0200

    Add yarn plugin

---
+++ plugins/yarn/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-yarn-installer
+# Description	: Enables oh-my-zsh's yarn plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "yarn"
