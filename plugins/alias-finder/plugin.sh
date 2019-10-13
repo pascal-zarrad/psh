7598be7a76ab3b3adc5d94f2e1ac16cb3e1fd9b8
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 02:30:41 +0200

    Add alias-finder plugin

---
+++ plugins/alias-finder/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-alias-finder-installer
+# Description	: Enables oh-my-zsh's alias finder
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "alias-finder"
