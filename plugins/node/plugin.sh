98cb35ca513ef3c4e8316f6938a70143489591df
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:17:11 +0200

    Add npm plugin

--- plugins/node/plugin.sh
+++ plugins/node/plugin.sh
@@ -2,10 +2,11 @@
 
 #==================================================================
 # Script Name   : psh-node-installer
-# Description	: Enables oh-my-zsh's node plugin
+# Description	: Enables oh-my-zsh's node and npm plugin
 # Args          : -
 # Author       	: Pascal Zarrad
 # Email         : P.Zarrad@outlook.de
 #==================================================================
 
 apply_antigen_bundle "node"
+apply_antigen_bundle "npm"
