c14d0ea42538fb174e579415b07777267977f26a
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:23:49 +0200

    Add ubuntu plugin

---
+++ plugins/ubuntu/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-ubuntu-installer
+# Description	: Enables oh-my-zsh's ubuntuubuntu plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "ubuntu"
