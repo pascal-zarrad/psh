70178187768a4f780028189919fc1c18f4cd016a
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:22:38 +0200

    Add symfony plugin

---
+++ plugins/redis-cli/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-redis-cli-installer
+# Description	: Enables oh-my-zsh's redis-cli plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "redis-cli"
