1e2b3caa378f8ced432d42b917a41e06613ae83b
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:21:06 +0200

    Add python plugin

---
+++ plugins/python/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-python-installer
+# Description	: Enables oh-my-zsh's python plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "python"
