f5c1c5ab5f72da5c059e3864f66d68423b090970
Pascal Zarrad <P.Zarrad@outlook.de>
2019-10-13 03:06:24 +0200

    Add git plugin

---
+++ plugins/git/plugin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#==================================================================
+# Script Name   : psh-git-installer
+# Description	: Enables oh-my-zsh's git plugin
+# Args          : -
+# Author       	: Pascal Zarrad
+# Email         : P.Zarrad@outlook.de
+#==================================================================
+
+apply_antigen_bundle "git"
