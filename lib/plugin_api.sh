75244e374d87447ad5b86b25d51769b278acb111
Pascal Zarrad <P.Zarrad@outlook.de>
2019-11-17 02:36:02 +0100

    Update comments

--- lib/plugin_api.sh
+++ lib/plugin_api.sh
@@ -11,6 +11,8 @@
 
 # Function that writes everything from the first parameter
 # to the .zshrc that is being generated
+#
+# @param $1 The line of content to write to the .zshrc
 function write_zshrc() {
     content="$1"
     echo "$content" >> "${HOME}/.zshrc"
@@ -18,6 +20,8 @@ function write_zshrc() {
 
 # Function that checks if antigen bundle is loaded or not
 # and then adds the bundle to the .zshrc
+#
+# @param $1 The bundle to add to the .zshrc
 function apply_antigen_bundle() {
     antigen_bundle="$1"
     if ! grep -q "$antigen_bundle" "${HOME}/.zshrc" ; then
@@ -27,9 +31,11 @@ function apply_antigen_bundle() {
 
 # Function that checks if antigen theme is loaded or not
 # and then adds the theme to the .zshrc
+#
+# @param $1 The name if the theme that should be added to the zshrc
 function apply_antigen_theme() {
-    antigen_bundle="$1"
-    if ! grep -q "antigen theme ${antigen_bundle}" "${HOME}/.zshrc" ; then
-        write_zshrc "antigen theme ${antigen_bundle}"
+    antigen_theme="$1"
+    if ! grep -q "antigen theme" "${HOME}/.zshrc" ; then
+        write_zshrc "antigen theme ${antigen_theme}"
     fi
 }
