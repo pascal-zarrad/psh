f6e8fdd6a3ad9829e7de73d3e6552d7e53c4834a
Pascal Zarrad <p.zarrad@outlook.de>
2022-03-10 00:02:42 +0100

    feat: always use the simpler "bira" theme

--- plugins/theme-loader/plugin.sh
+++ plugins/theme-loader/plugin.sh
@@ -25,12 +25,4 @@
 # Email         : P.Zarrad@outlook.de
 #==================================================================
 
-# As WSL can install powerline fonts, they need also be installed on windows itself.
-# So we just check if we're running on WSL and use another good font.
-if grep -q "Microsoft" "/proc/version" || grep -q ".*microsoft-standard*." "/proc/version"
-    then
-        echo "Running on Windows Subsystem for Linux, falling back to bira theme!"
-        apply_antigen_theme "bira"
-    else
-        apply_antigen_theme "agnoster"
-fi
+apply_antigen_theme "bira"
