c9a853a684a917968063edf87f44ccc5fad2d821
Pascal Zarrad <p.zarrad@outlook.de>
2022-03-13 17:23:56 +0100

    feat: update copyright notice

--- plugins/command-not-found/plugin.sh
+++ plugins/command-not-found/plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright 2021 Pascal Zarrad
+# Copyright 2022 Pascal Zarrad
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
