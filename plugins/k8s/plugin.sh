62ed30d958c3a616cca860f20cc5c92f160482ed
Pascal Zarrad <p.zarrad@outlook.de>
2024-03-25 19:16:56 +0100

    chore: update copyright notice

--- plugins/k8s/plugin.sh
+++ plugins/k8s/plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright 2022 Pascal Zarrad
+# Copyright 2024 Pascal Zarrad
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
