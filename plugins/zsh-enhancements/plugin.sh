78ef189b164392928723b950a41809d77b606a7f
Pascal Zarrad <p.zarrad@outlook.de>
2024-07-26 21:48:29 +0200

    feat: downgrade autocomplete due to issues

--- plugins/zsh-enhancements/plugin.sh
+++ plugins/zsh-enhancements/plugin.sh
@@ -34,7 +34,16 @@ apply_plugin "zsh-users/zsh-completions"
 # It suggests commands as you type based on history and completions.
 apply_plugin "zsh-users/zsh-autosuggestions"
 
+# This is a clean-room implementation of the Fish shell's history
+# search feature, where you can type in any part of any command
+# from history and then press chosen keys, such as the UP and
+# DOWN arrows, to cycle through matches.
+apply_plugin "zsh-users/zsh-history-substring-search"
+
 # This plugin adds autocompletion to further speed up command
 # line work. Together with autosuggestions, it makes typing
 # commands a breeze.
-apply_plugin "marlonrichert/zsh-autocomplete"
+# The plugin has been locked to a specific commit as ~ expansion
+# on path autocomplete "broke" one commit after this one.
+# See: https://github.com/marlonrichert/zsh-autocomplete/issues/741
+apply_plugin_version "marlonrichert/zsh-autocomplete" "a66d6ae6f7e703b1bf5ae57e48a7da3cf307aeab"
