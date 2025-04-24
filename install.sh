66cd85669ec9578b3e846f08740eee2ce4e221a4
Pascal Zarrad <p.zarrad@outlook.de>
2025-04-24 21:56:37 +0200

    chore: bump version to v3.0.0

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@
 #==================================================================
 
 # Semantic versioning version constant
-readonly PSH_VERSION="2.2.2"
+readonly PSH_VERSION="3.0.0"
 echo "PSH - VERSION: ${PSH_VERSION}"
 
 if [ -f /etc/os-release ]; then
